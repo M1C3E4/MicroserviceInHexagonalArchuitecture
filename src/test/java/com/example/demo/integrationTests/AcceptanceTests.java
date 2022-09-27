b48b193d14ed493d2ba102d192dd548420b68f5b
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-27 09:33:20 +0100

    added stage red in TDD

--- src/test/java/com/example/demo/integrationTests/AcceptanceTests.java
+++ src/test/java/com/example/demo/integrationTests/AcceptanceTests.java
@@ -1,4 +1,28 @@
 package com.example.demo.integrationTests;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AcceptanceTests {
+    @Test
+    public void contextLoad() {
+    }
+
+    @Test
+    @DisplayName("https://localhost:9090/findById/{id} -> 200")
+    public void shouldReturnMovieById() {
+        fail();
+    }
+
+    @Test
+    @DisplayName("https://localhost:9090/allmovie -> 200")
+    public void shouldReturnAllMovie() {
+        fail();
+    }
 }
